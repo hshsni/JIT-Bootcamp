897c2904df1b7bfdac6852f0fbf6f5a21e7d2132
saaketh <saakethaj@gmail.com>
2020-12-19 17:33:14 +0530

    register page done

---
+++ firebase_init.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGAXMyf2aGePqv5M-jgKrLKv2kOv-801o",
+  authDomain: "hackathon-2020-jit.firebaseapp.com",
+  projectId: "hackathon-2020-jit",
+  storageBucket: "hackathon-2020-jit.appspot.com",
+  messagingSenderId: "191351955645",
+  appId: "1:191351955645:web:3366dabde5438b737b17d4",
+  measurementId: "G-4WZ23JDJ6X",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firebaseRealtimeDatabase = firebase.database();
